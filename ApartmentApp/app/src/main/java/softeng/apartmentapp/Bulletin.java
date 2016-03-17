77c914c1c963a80184facb5c0e4fd83d1e1143cb
nthiessen <nick.a.d.thiessen@gmail.com>
2016-03-16 21:21:28 -0500

    Added listview to bulletin
    also removed that dumb email icon from everything

--- ApartmentApp/app/src/main/java/softeng/apartmentapp/Bulletin.java
+++ ApartmentApp/app/src/main/java/softeng/apartmentapp/Bulletin.java
@@ -6,8 +6,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bulletin extends AppCompatActivity {
+    List<String> data;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +24,26 @@ public class Bulletin extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        fillList();
     }
 
+    protected void fillList() {
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+        data = new ArrayList<>();
+        data.add("Title");
+        data.add("Dorum Lipsum Something Something...");
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, android.R.id.text1, data);
+
+        listView.setAdapter(adapter);
+    }
+
+    public void addBulletin(View view) {
+        data.add("HI!");
+        data.add("Stuff goes here I guess");
+        adapter.notifyDataSetChanged();
+    }
 }
