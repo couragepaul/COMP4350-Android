c0671bd243c2de7f768ad05da8e0661e3057b33c
nthiessen <nick.a.d.thiessen@gmail.com>
2016-03-16 22:33:58 -0500

    Added day listview
    ^ that

--- ApartmentApp/app/src/main/java/softeng/apartmentapp/Calendar.java
+++ ApartmentApp/app/src/main/java/softeng/apartmentapp/Calendar.java
@@ -1,13 +1,18 @@
 package softeng.apartmentapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class Calendar extends AppCompatActivity {
+
+    CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +20,21 @@ public class Calendar extends AppCompatActivity {
         setContentView(R.layout.activity_calendar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int day, int month, int year) {
+                openDay();
+            }
+        });
+    }
+
+    public void openDay() {
+        Intent intent = new Intent(this, Day.class);
+        startActivity(intent);
     }
 
 }
