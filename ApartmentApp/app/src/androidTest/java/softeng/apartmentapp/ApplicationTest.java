ad9c2d15feba9073740d6d15ad9f6e1e84b86e47
Courage <umeboigb@myumanitoba.ca>
2016-03-18 17:22:51 -0500

    Update Application.java

--- ApartmentApp/app/src/androidTest/java/softeng/apartmentapp/ApplicationTest.java
+++ ApartmentApp/app/src/androidTest/java/softeng/apartmentapp/ApplicationTest.java
@@ -10,10 +10,10 @@ import com.robotium.solo.Solo;
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
 
-    private static  String username = "Note 1";
-    private static  String password = "Note 2";
+    private static  String username = "username";
+    private static  String password = "password";
 
-    private Solo solo = new Solo(null);
+    
     public ApplicationTest() {
         super(Application.class);
     }
@@ -28,26 +28,13 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
 
 
     @Override
-    public void tearDown() throws Exception {
+    public void tearDown() throws Exception
+    {
         //tearDown() is run after a test case has finished.
         //finishOpenedActivities() will finish all the activities that have been opened during the test execution.
-        solo.finishOpenedActivities();
     }
 
-    public void test_login() {
-        solo.enterText(0, username);
-        solo.enterText(1, password);
-        solo.clickOnButton("Login");
-        assertTrue(solo.searchText("Please wait. Logging in."));
-        solo.waitForActivity("com.pointabout.personal.MainTabActivity", 3000);
-        solo.assertCurrentActivity("The activity should be Main Tab", "MainTabActivity");
-        solo.sendKey(Solo.MENU);
-        solo.clickOnText("Logout");
-        solo.waitForText("Are you sure you want to log out");
-        solo.clickOnButton("Logout");
-        solo.waitForText("You have been logged out of Personal.");
-
-    }
+    public void test_login() {}
 
 
 }
