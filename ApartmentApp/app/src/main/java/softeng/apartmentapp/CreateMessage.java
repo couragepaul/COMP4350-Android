02640a04d9e18ed91c794e6c031a5daed7c39b9b
Light <lightkeeper7mail@gmail.com>
2016-03-18 11:27:18 -0500

    Front-end for android have been completed - You can now click on individual messages to see them individually - You can create new messages

--- ApartmentApp/app/src/main/java/softeng/apartmentapp/CreateMessage.java
+++ ApartmentApp/app/src/main/java/softeng/apartmentapp/CreateMessage.java
@@ -24,15 +24,6 @@ public class CreateMessage extends AppCompatActivity {
         setContentView(R.layout.activity_create_message);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
--- ApartmentApp/app/src/main/java/softeng/apartmentapp/Messages.java
+++ ApartmentApp/app/src/main/java/softeng/apartmentapp/Messages.java
@@ -8,8 +8,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import softeng.restAndObjects.Message;
@@ -17,6 +19,7 @@ import softeng.restAndObjects.Message;
 import static softeng.restAndObjects.RestInterface.getMessages;
 
 public class Messages extends AppCompatActivity {
+    private ArrayList<Message> msges = new ArrayList<Message>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,7 @@ public class Messages extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        ArrayList<Message> msges = new ArrayList<Message>();
+        msges = new ArrayList<Message>();
         msges = getMessages("COMP4350_SU"); // testing
         String[] items = new String[msges.size()];
         for(int i=0; i < msges.size(); i++) {
@@ -41,13 +44,33 @@ public class Messages extends AppCompatActivity {
         }
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.content_messages,R.id.Title,items);
-        ListView lv = (ListView) findViewById(R.id.msgList);
+        final ListView lv = (ListView) findViewById(R.id.msgList);
         lv.setAdapter(adapter);
-    }
 
+        //onclick listener
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedFromList =(String) (lv.getItemAtPosition(position));
+                //go to view the message Page
+                viewMessage(view,msges.get(position));
+            }
+
+        });
+    }
 
     public void createNewMessage(View view) {
         Intent intent = new Intent(this, CreateMessage.class);
+        startActivity(intent);
+    }
+
+    public void viewMessage(View view, Message msg) {
+        Intent intent = new Intent(this, ViewMessage.class);
+        Bundle extras = new Bundle();
+        extras.putString("sender",msg.sender);
+        extras.putInt("timestamp", msg.timestamp);
+        extras.putString("content", msg.content);
+        intent.putExtras(extras);
         startActivity(intent);
     }
 }
